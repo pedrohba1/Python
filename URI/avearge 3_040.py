ce0fbec86abe6b4ab7f9a4078a66336cc9d977d7
pedrohba1 <pedro.almeida@ufu.br>
2018-06-15 13:36:00 -0300

    exercicio 1040 do uri foi adicionado

---
+++ URI/avearge 3_040.py
@@ -0,0 +1,30 @@
+
+x = input()
+l = [float(i) for i in x.split()]
+lp = [2,3,4,1]
+media = 0.0
+for i in range(4):
+	media += l[i] * lp[i]
+
+media = media/10 
+
+print("Media: %.1f" % media)
+
+if(media>=7.0):
+	print("Aluno aprovado.")
+
+if(media<5.0):
+	print("Aluno reprovado.")
+
+if(media>=5.0 and media<=6.9):
+	print("Aluno em exame.")
+	nota = float(input())
+	media += nota
+	media = media/2
+	print("Nota do exame: %.1f" % nota)
+	if(media<5.0):
+		print("Aluno reprovado.")
+		print("Media final: %.1f" % media)
+	if(media>= 5.0):
+		print("Aluno aprovado.")
+		print("Media final: %.1f" % media)
